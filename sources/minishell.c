cfd7344d3841926e7ab2c7e2a6649c114dde16dc
sathyagimenes <sathya.gimenes@gmail.com>
2023-03-19 15:42:21 -0300

    fix: norme in main

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,18 +6,19 @@
 /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 12:34:43 by sde-cama          #+#    #+#             */
-/*   Updated: 2023/03/19 15:35:34 by sde-cama         ###   ########.fr       */
+/*   Updated: 2023/03/19 15:41:25 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	if (ft_check_main_args(argc, argv))
 		return (ERROR_CODE);
 	// depois excluir if da envp
 	if (!envp)
 		return (ERROR_CODE);
+	// salvar envp em uma hash table
 	return (SUCCESS_CODE);
 }
